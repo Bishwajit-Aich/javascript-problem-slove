2a9492d3fa2341747c0cbe714c15124a1dabb68b
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-11-24 00:27:49 +0600

    Add files via upload

---
+++ problem6.js
@@ -0,0 +1,111 @@
+/*
+ * You and your friends Tom,Jane,Peter and John got their final exam results.Your Total score is 85,Tom's Total score is 66, Jane's total score is 95, Peter's total score is 56 and John's Total score is 40.
+        The Grading Chart is:
+            80 or above A Grade.
+            60 or above B Grade.
+            50 or above C Grade.
+            40 or above D Grade.
+            39 or above => F Grade.
+
+   Write a program to find your and your friends grades using if-else.         
+
+ */
+   var myScore = 85;
+   var tomScore = 66;
+   var janeScore = 95;
+   var peterScore = 56;
+   var johnScore = 40;
+
+//    My Score
+   if( myScore >= 80){
+    console.log('Congratulations! You Got A!')
+   }
+   else if( myScore >= 60){
+    console.log('Congratulations! You Got B!')
+   }
+   else if( myScore >= 50){
+    console.log('Congratulations! You Got C!')
+   }
+   else if( myScore >= 40){
+    console.log('Congratulations! You Got D!')
+   }
+   else{
+    console.log('Sorry! You Are Fail!');
+   }
+
+// Tom Score
+   if( tomScore >= 80){
+    console.log('Congratulations! Tom You Got A!')
+   }
+   else if( tomScore >= 60){
+    console.log('Congratulations! Tom You Got B!')
+   }
+   else if( tomScore >= 50){
+    console.log('Congratulations! Tom You Got C!')
+   }
+   else if( tomScore >= 40){
+    console.log('Congratulations! Tom You Got D!')
+   }
+   else{
+    console.log('Sorry! Tom You Are Fail!');
+   }
+
+   
+   
+//    Jane Score
+   if( janeScore >= 80){
+    console.log('Congratulations! Jane You Got A!')
+   }
+   else if( janeScore >= 60){
+    console.log('Congratulations! Jane You Got B!')
+   }
+   else if( janeScore >= 50){
+    console.log('Congratulations! Jane You Got C!')
+   }
+   else if( janeScore >= 40){
+    console.log('Congratulations! Jane You Got D!')
+   }
+   else{
+    console.log('Sorry! Jane You Are Fail!');
+   }
+
+
+
+// Peter Score
+   if( peterScore >= 80){
+    console.log('Congratulations! peter You Got A!')
+   }
+   else if( peterScore >= 60){
+    console.log('Congratulations! peter You Got B!')
+   }
+   else if( peterScore >= 50){
+    console.log('Congratulations! peter You Got C!')
+   }
+   else if( peterScore >= 40){
+    console.log('Congratulations! peter You Got D!')
+   }
+   else{
+    console.log('Sorry! peter You Are Fail!');
+   }
+
+
+
+// John's Score
+   if( johnScore >= 80){
+    console.log('Congratulations! Johns You Got A!')
+   }
+   else if( johnScore >= 60){
+    console.log('Congratulations! Johns You Got B!')
+   }
+   else if( johnScore >= 50){
+    console.log('Congratulations! Johns You Got C!')
+   }
+   else if( johnScore >= 40){
+    console.log('Congratulations! Johns You Got D!')
+   }
+   else{
+    console.log('Sorry! Johns You Are Fail!');
+   }
+
+
+  
