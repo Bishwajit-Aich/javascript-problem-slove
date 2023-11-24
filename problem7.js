6e776b1e8916b3ba4350dc5333f6119b4802e80a
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-11-24 19:10:47 +0600

    Add files via upload

---
+++ problem7.js
@@ -0,0 +1,24 @@
+/**
+ * You are give three numbers 13,79 and 45. Write a Program that will print the largest number using if-else
+ */
+
+var firstNumber = 13;
+var secondNumber = 79;
+var thirdNumber = 45;
+
+
+// First Number
+if(firstNumber > secondNumber &&  firstNumber > thirdNumber){
+    console.log('Largest Number Is 13!');
+}
+
+// Second Number
+if(secondNumber > firstNumber && secondNumber > thirdNumber){
+    console.log('The largest Number Is 79!');
+}
+
+//Third Number
+
+if(thirdNumber > firstNumber && thirdNumber > secondNumber){
+    console.log('The Largest Number Is 45!');
+}
