346b62ac95aa66f4467fc6b4e8a95e8022547153
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-11-24 19:21:43 +0600

    Add files via upload

---
+++ problem8.js
@@ -0,0 +1,21 @@
+/**
+ * You Are Given a triangle with the sides 9,8,9.Write a program to check whether a triangle is Isosceles or not using if-else.
+            Isosceles ==> Two Sides Are Equal.
+ */
+
+            var firstSide = 10;
+            var secondSide = 10;
+            var thirdSide = 2;
+
+            if(firstSide === secondSide){
+                console.log("firstside and secondside Two Sides Are Equal!");
+            }
+
+            else if(secondSide === thirdSide ){
+                console.log("second side and third side Two Sides Are Equal!");
+            }
+            else if(thirdSide === firstSide){
+                console.log("First Side And Third Side bot side are equal");
+            }
+         
+
