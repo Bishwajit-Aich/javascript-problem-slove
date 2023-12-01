97c0d6b593c5a30c6f79576d83c3c52b124eb5ad
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-12-01 19:29:07 +0600

    Add files via upload

---
+++ problem10.js
@@ -0,0 +1,10 @@
+/**
+ * Write A Program to Display 1-100 Even Numbers Using While Loop!
+ */
+
+var evenNumbers = 1;
+
+while(evenNumbers <= 100){
+    console.log(evenNumbers);
+    evenNumbers += 2;
+}
---
+++ problem11.js
@@ -0,0 +1,11 @@
+/**
+ * Write A Program to Display 1-100 Odd Numbers Using While Loop!
+ */
+
+
+var oddNumbers = 1;
+
+while(oddNumbers < 100){
+    console.log(oddNumbers);
+    oddNumbers+=2;
+}
