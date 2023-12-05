8288606332a7a696b25753da95aa9714a8b22538
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-12-05 11:52:29 +0600

    For Loop problem slove

---
+++ for.js
@@ -0,0 +1,23 @@
+// For Loop
+
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('Roast Den Please');
+// }
+
+// 1-10 print using for loop
+
+// for(var numbers = 1; numbers <=10; numbers++){
+//     console.log('The Number Is',numbers);
+// }
+
+// 1-10 even numbers using for loop
+
+// for(var evenNumbers = 0; evenNumbers <= 10; evenNumbers+=2){
+//     console.log('The Even Number Is : ',evenNumbers);
+// }
+
+// 1-10 odd Numbers Using for loop
+
+// for(var oddNumbers = 1; oddNumbers <= 10; oddNumbers+=2){
+//     console.log('The Odd Number Is : ',oddNumbers);
+// }
