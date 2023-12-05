08bd6d3329ff55456968cc5328c450128bde6dd8
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-12-05 12:12:27 +0600

    array element shows using for loop

---
+++ problem12.js
@@ -0,0 +1,6 @@
+//Display Every Elements of an array using for loop
+
+var numbers = [45 , 52 , 47 , 85 , 96 , 63,85,96,63];
+for(var i = 0; i < numbers.length; i++){
+   console.log(numbers[i]);
+}
